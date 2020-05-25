8212704fecde5fa1acbb49dbc8dd45632f0b8250
Edcantut <33295170+Edcantut@users.noreply.github.com>
2020-05-25 04:20:35 -0500

    poder

--- views/miEquipo.php
+++ views/miEquipo.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html> 
-<html LANG="en"> 
-    <head> 
+<!DOCTYPE html>
+<html LANG="en">
+    <head>
     <meta charset="utf-8"/>
         <link href="./ASSETS/CSS/loginCSS.css" rel="stylesheet" />
-        <title>Mi </title> 
-    </head> 
-    <body>  
+        <title>Mi </title>
+    </head>
+    <body>
     <script type ="text/javascript" src="./CONTROLLER/miEquipoController.js"></script>
     <h1 class="display-1 text-center">Mi equipo</h1>
     <br>
@@ -14,14 +14,14 @@
             <div class="col-3"></div>
             <div class="col-6">
                 <div id="divPeriodos" style="background-color:white; text-align: center;">
-                            <?php 
+                            <?php
                                 $id = $_SESSION['id'];
                                 $sql = "SELECT * FROM equipovoluntario WHERE equipovoluntario.IDvoluntario ='$id'";
 		                        $consulta = mysqli_query ($conexion,$sql) or die ("Fallo en la consulta ".$sql);
 	                        	$nfilas = mysqli_num_rows ($consulta);
 
                                 if($nfilas <1){
-                                    echo "<h4>Aun no perteneces a ningun equipo, asegurate de haber registrado tu horario.</h4>"; 
+                                    echo "<h4>Aun no perteneces a ningun equipo. Si ya registraste tu horario y estas en un periodo es cuestion de espera.</h4>"; 
                                 }else{
                                     $tupla = mysqli_fetch_array ($consulta);
                                     $idEquipo = $tupla['IDequipo'];
@@ -29,15 +29,15 @@
                                     $sql = "SELECT ev.IDequipo as idE,v.Nombre as nombre, v.Correo as correo, v.Celular as celular FROM equipovoluntario as ev,voluntario as v WHERE ev.IDvoluntario = v.IDvoluntario AND ev.IDequipo = '$idEquipo' ";
 		                            $consulta = mysqli_query ($conexion,$sql) or die ("Fallo en la consulta ".$sql);
 	                        	    $nfilas = mysqli_num_rows ($consulta);
-                                      echo "<h3>Integrantes de tu equipo ($idEquipo)</h3><br>"; 
+                                      echo "<h3>Integrantes de tu equipo ($idEquipo)</h3><br>";
                                      for ($i=0; $i<$nfilas; $i++)
 		                             {
 			                        $tupla = mysqli_fetch_array ($consulta);
                                     $nombre = $tupla['nombre'];
                                     $correo = $tupla['correo'];
                                     $celular = $tupla['celular'];
-                                         echo "<strong> Nombre:</strong> $nombre      <strong>Correo:</strong>$correo     <strong>Celular:</strong>$celular"; 
-			                             echo "<br>"; 
+                                         echo "<strong> Nombre:</strong> $nombre      <strong>Correo:</strong>$correo     <strong>Celular:</strong>$celular";
+			                             echo "<br>";
 		                             }
 
                                 }
@@ -47,6 +47,6 @@
             <div class="col-3"></div>
         </div>
     </div>
-     
-    </body> 
-</html> 
+
+    </body>
+</html>
