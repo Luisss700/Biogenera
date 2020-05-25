8b16ce55613583447bb6525eec028111633ee288
Edcantut <33295170+Edcantut@users.noreply.github.com>
2020-05-25 04:30:12 -0500

    x

--- views/registro.php
+++ views/registro.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html LANG="en">
     <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta charset="utf-8"/>
         <title>Registro</title>
     </head>
     <body>
