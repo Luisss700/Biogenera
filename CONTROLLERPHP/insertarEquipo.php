470e6996232b676659f18ba6dabfe0d0218cd420
Edcantut <33295170+Edcantut@users.noreply.github.com>
2020-05-25 02:22:16 -0500

    Funcionan insertr, solo se ve voluntarios del periodo.Solo faltaelborrar

--- CONTROLLERPHP/insertarEquipo.php
+++ CONTROLLERPHP/insertarEquipo.php
@@ -1,12 +1,12 @@
 <?php
-$id = $_POST['registrarEquipo'];
+$id = $_POST['positivo'];
 $equipo=$_POST['equipo'];
 $tabla='equipovoluntario';
 $columnas= array("IDequipo","IDvoluntario");
 $valores=array();
 require("insert.php");
 $Insert=new Insert;
-array_push($valores,$id,$equipo);
+array_push($valores,$equipo,$id);
 $Insert->insertar($tabla,$columnas,$valores);
 
  ?>
--- views/editarEquipos.php
+++ views/editarEquipos.php
@@ -1,7 +1,11 @@
 <?php
-if(isset($_POST['registrarEquipo']))
+if(isset($_POST['positivo']))
 {
     require ("CONTROLLERPHP/insertarEquipo.php");
+}
+if(isset($_POST['negativo']))
+{
+    require ("CONTROLLERPHP/borrarEquipo.php");
 }
 
  ?>
@@ -62,6 +66,7 @@ if(isset($_POST['registrarEquipo']))
                             echo "  <option  value = '$id' data-nombre='$id' >$id</option>  ";
                     }
               echo '</select>';
+             echo "<input type='hidden' name='periodo' value='".@$periodo."'>";
 
           ?>
              <br>
@@ -73,7 +78,7 @@ if(isset($_POST['registrarEquipo']))
            if (@$_POST["Fase"]==2) {
          @$equipo=$_POST['equipo'];
          @$periodo=$_POST['periodo'];
-         $sql = "SELECT * FROM voluntario WHERE IDvoluntario NOT IN(SELECT IDvoluntario FROM equipovoluntario) ";
+         $sql = "SELECT * FROM voluntario WHERE IDvoluntario IN(SELECT IDvoluntario FROM voluntario WHERE IDvoluntario NOT IN(SELECT IDvoluntario FROM equipovoluntario)) AND IDperiodo=$periodo ";
          $consulta = mysqli_query ($conexion,$sql) or die ("Fallo en la consulta ".$sql);
          $nfilas = mysqli_num_rows ($consulta);
          ?>
@@ -91,17 +96,49 @@ if(isset($_POST['registrarEquipo']))
             <?php
             for ($i=0; $i<$nfilas; $i++) {
             $tupla = mysqli_fetch_array($consulta);
+
             echo "<tr>";
             echo  "<td>".$tupla['Nombre']."</td>";
-            echo  "<td><button type='submit' class='btn btn-primary' name='registrarEquipo' value='".$tupla['IDvoluntario']."'>Agregar</button></td>";
+            echo  "<td><button type='submit' class='btn btn-primary' name='positivo' value='".$tupla['IDvoluntario']."'>Agregar</button></td>";
             echo "</tr>";
             echo "<input type='hidden' name='periodo' value='".@$periodo."'>";
             echo "<input type='hidden' name='equipo' value='".@$equipo."'>";
           }
             ?>
           </tbody>
+        </table>
 
-         </table>
+        <?php
+        $sql2 = "SELECT IDvoluntario FROM equipovoluntario WHERE IDequipo=$equipo ";
+        $consulta2 = mysqli_query ($conexion,$sql2) or die ("Fallo en la consulta ".$sql);
+        $nfilas2 = mysqli_num_rows ($consulta2);
+         ?>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Eliminar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            for ($i=0; $i<$nfilas2; $i++) {
+           $tupla2 = mysqli_fetch_array($consulta2);
+           $idvoluntario2=$tupla2['IDvoluntario'];
+           $sql="SELECT * FROM voluntario WHERE IDvoluntario=$idvoluntario2";
+           $consulta = mysqli_query ($conexion,$sql) or die ("Fallo en la consulta ".$sql);
+           $tupla3 = mysqli_fetch_array($consulta);
+           echo "<tr>";
+           echo  "<td>".$tupla3['Nombre']."</td>";
+           echo  "<td><button type='submit' class='btn btn-primary' name='negativo' value='".$tupla3['IDvoluntario']."'>Eliminar</button></td>";
+           echo "</tr>";
+           echo "<input type='hidden' name='periodo' value='".@$periodo."'>";
+           echo "<input type='hidden' name='equipo' value='".@$equipo."'>";
+         } ?>
+          </tbody>
+        </table>
+
+
         </div>
         <input type="hidden" name="ruta" value="editarEquipos">
         <input type="hidden" name="Fase" value="2">
